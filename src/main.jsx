52dcbf9e298652ce66356c2ce9993606395a0ed6
ElloRabyndra <Ellorabyndra123@gmail.com>
2025-08-04 00:05:22 +0700

    chore: setup folder structure and theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./style.css";
-import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter } from "react-router"; // Imports BrowserRouter for client-side routing
+import { AuthProvider } from "./features/auth/useAuth"; // Imports AuthProvider for authentication context
+import ThemeProvider from "./context/ThemeContext"; // Imports ThemeProvider for theme management
+import ToastWrapper from "./components/common/ToastWrapper"; // Imports ToastWrapper for displaying toast notifications
+import Routing from "./routing/Routing"; // Imports Routing component which defines application routes
+import "./style/Style.css"; // Imports global CSS styles
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <ToastWrapper />
+          <Routing />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>
 );
